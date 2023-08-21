ccd37f5ba5d0566f69f40dc5d1d711cde81e90d1
Paul Henderson <paulh6819@gmail.com>
2023-08-21 18:16:33 -0400

    connected googlebooksAPI with booksrun API

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const booksRunApiKey = "p83zv395qxgyr2mj7xsn";
 
 const apiKEY = "AIzaSyDklC7lbmUzVOIMCUMEhbas-WTu5AYG94c";
 
-const restOrUrl = `/books/v1/volumes\?q\=flowers+inauthor:keyes\&key\=${apiKEY}`;
-
 const app = express();
 
 const port = 4006;
@@ -22,22 +20,20 @@ const port = 4006;
 
 app.get("/", async (req, res) => {
   try {
-    const { data } = await axios.get(
-      `${baseURL}/books/v1/volumes?q=intitle:"Pharmacology+in+Nursing+Fourteenth+Edition"&key=${apiKEY}`,
-      `${baseBooksRunURL}{9781595829788}?key=${booksRunApiKey}`
+    const googleData = await axios.get(
+      `${baseURL}/books/v1/volumes?q=intitle:"If on a winter's night a traveler"&key=${apiKEY}`
     );
-
-    console.log(data.items);
 
     // Then, we extract the ISBN_13 as before
     let isbn13 = null;
     if (
-      data.items &&
-      data.items[0] &&
-      data.items[0].volumeInfo &&
-      data.items[0].volumeInfo.industryIdentifiers
+      googleData.data.items &&
+      googleData.data.items[0] &&
+      googleData.data.items[0].volumeInfo &&
+      googleData.data.items[0].volumeInfo.industryIdentifiers
     ) {
-      for (const identifier of data.items[0].volumeInfo.industryIdentifiers) {
+      for (const identifier of googleData.data.items[0].volumeInfo
+        .industryIdentifiers) {
         if (identifier.type === "ISBN_13") {
           isbn13 = identifier.identifier;
           break;
@@ -45,8 +41,18 @@ app.get("/", async (req, res) => {
       }
     }
 
-    // Sending the ISBN_13 back to the client or handle as needed
-    res.json({ isbn13 });
+    let booksrunData = null;
+
+    if (isbn13) {
+      booksrunData = await axios.get(
+        `${baseBooksRunURL}${isbn13}?key=${booksRunApiKey}`
+      );
+    }
+
+    res.json({
+      googleData: isbn13, // Changed this line
+      booksRunData: booksrunData ? booksrunData.data : null,
+    });
   } catch (error) {
     console.error("error fetching data", error);
     res.status(500).send("internal server error");
